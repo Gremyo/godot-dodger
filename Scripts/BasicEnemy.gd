a1134504b32e7dc00246c8d1ae22d0f3436532c2
Robert Dixon <attrition242@gmail.com>
2022-02-17 15:07:42 -0500

    More decoupling for future enemy types and instances

--- Scripts/BasicEnemy.gd
+++ Scripts/BasicEnemy.gd
@@ -1,14 +1,12 @@
 extends RigidBody2D
 
-var direction
 var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+var rotation_variation = rand_range(-PI / 4, PI / 4)
 
 func _ready():
 	$AnimatedSprite.animation = "walk"
 	$AnimatedSprite.playing = true
-	rotation = direction
-	linear_velocity = velocity.rotated(direction)
-
+	linear_velocity = velocity.rotated(rotation)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -14,6 +14,15 @@ func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+func mob_instantiation(mob_index,spawn_position,spawn_rotation):
+	# Create a Mob instance and add it to the scene.
+	# the mobspawnlocation path follower rotates to be parralel with the path
+	# so we rotate to perpendicularity and then add a variation
+	var mob = $MobScenes.get_mob(mob_index).instance()
+	mob.position = spawn_position
+	mob.rotation = spawn_rotation + PI / 2 + mob.rotation_variation
+	return mob
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -25,10 +34,5 @@ func _on_ScoreTimer_timeout():
 func _on_MobTimer_timeout():
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation");
 	mob_spawn_location.offset = randi()
-
-	# Create a Mob instance and add it to the scene.
-	var mob = $MobScenes.get_mob(0).instance()
-	mob.position = mob_spawn_location.position
-	mob.direction = mob_spawn_location.rotation + PI / 2
-	mob.direction += rand_range(-PI / 4, PI / 4)
+	var mob = mob_instantiation(0,mob_spawn_location.position,mob_spawn_location.rotation)
 	add_child(mob)
